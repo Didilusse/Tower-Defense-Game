e3f9db766e71ba745342ce4e9bcb316445471953
Adil Rahmani <didilusse@gmail.com>
2024-09-26 16:31:52 -0400

    bug fixes and drawing sprites
    I was able to fix a bug where you can't see anything unless u resiz. the window. I was also able to spawn some sprites on the grid in a random way/

--- src/Game.java
+++ src/Game.java
@@ -1,17 +1,39 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class Game extends JFrame {
 
     private GameScreen gameScreen;
 
+    private BufferedImage img;
+
     public Game(){
 
+        importImg();
+
         setSize(640, 640);
-        setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
-        gameScreen = new GameScreen();
+        gameScreen = new GameScreen(img);
         add(gameScreen);
+        setVisible(true);
+
+        gameScreen = new GameScreen(img);
+        add(gameScreen);
+    }
+
+    private void importImg() {
+
+        InputStream is = getClass().getResourceAsStream("/spriteatlas.png");
+
+        try {
+            img = ImageIO.read(is);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static void main(String[] args) {
--- src/GameScreen.java
+++ src/GameScreen.java
@@ -1,24 +1,55 @@
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.JPanel;
+
+import java.awt.image.BufferedImage;
+import java.awt.Graphics;
+import java.awt.Color;
+
+import java.util.ArrayList;
 import java.util.Random;
 
 public class GameScreen extends JPanel {
 
     private Random random;
+    private BufferedImage img;
 
-    public GameScreen(){
+    private ArrayList<BufferedImage> sprites = new ArrayList<>();
+
+    public GameScreen(BufferedImage img){
+        this.img = img;
+
+        loadSprites();
+        
         random = new Random();
     }
+    //320x96
+    //function to load the sprites
+    private void loadSprites() {
+        for(int y = 0; y < 10; y++){
+            for (int x = 0; x < 10; x++){
+                //looping through the grid of the image
+                //adding every 32 x 32 pixels to the sprites
+                sprites.add(img.getSubimage(x * 32, y * 32, 32, 32));
+            }
+        }
+    }
+
     public void paintComponent(Graphics g){
         super.paintComponent(g);
+
+        //g.drawImage(sprites.get(19), 0, 0, this);
+
+
         for(int y = 0; y < 20; y++){
             for (int x = 0; x < 20; x++){
-                g.setColor(getRandomColor());
-                g.fillRect(x*32,y * 32,32,32);
+                g.drawImage(sprites.get(getRndInt(20)), x * 32, y * 32, null);
             }
         }
 
 
+    }
+
+    private int getRndInt(int max){
+        return random.nextInt(max);
     }
 
     private Color getRandomColor(){
