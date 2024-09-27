6b58234ee4966bc54489a0c18282db6394ddbada
Adil Rahmani <didilusse@gmail.com>
2024-09-26 21:15:11 -0400

    cleanup and stuff

--- src/Game.java
+++ src/Game.java
@@ -1,31 +1,31 @@
-import javax.imageio.ImageIO;
-import javax.swing.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 
-public class Game extends JFrame {
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+
+public class Game extends JFrame implements Runnable {
 
     private GameScreen gameScreen;
-
     private BufferedImage img;
+    private Thread gameThread;
 
-    private double timePerFrames;
-    private long lastFrame;
+    private final double FPS_SET = 120.0;
+    private final double UPS_SET = 60.0;
 
-    public Game(){
-        timePerFrames = 1000000000 / 60.0;
+    public Game() {
+
         importImg();
 
         setSize(640, 640);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
+
         gameScreen = new GameScreen(img);
         add(gameScreen);
         setVisible(true);
 
-        gameScreen = new GameScreen(img);
-        add(gameScreen);
     }
 
     private void importImg() {
@@ -35,24 +35,68 @@ public class Game extends JFrame {
         try {
             img = ImageIO.read(is);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
+
     }
 
-    private void gameLoop(){
-        while(true){
-            if(System.nanoTime() - lastFrame >= timePerFrames) {
-                lastFrame = System.nanoTime();
-                repaint();
-            }else{
+    private void start() {
+        gameThread = new Thread(this) {
+        };
 
-            }
-        }
+        gameThread.start();
     }
 
+    private void updateGame() {
+
+        // System.out.println("Game Updated!");
+    }
 
     public static void main(String[] args) {
+
         Game game = new Game();
-        game.gameLoop();
+        game.start();
+
     }
+
+    @Override
+    public void run() {
+
+        double timePerFrame = 1000000000.0 / FPS_SET;
+        double timePerUpdate = 1000000000.0 / UPS_SET;
+
+        long lastFrame = System.nanoTime();
+        long lastUpdate = System.nanoTime();
+        long lastTimeCheck = System.currentTimeMillis();
+
+        int frames = 0;
+        int updates = 0;
+
+        while (true) {
+
+            // Render
+            if (System.nanoTime() - lastFrame >= timePerFrame) {
+                repaint();
+                lastFrame = System.nanoTime();
+                frames++;
+            }
+
+            // Update
+            if (System.nanoTime() - lastUpdate >= timePerUpdate) {
+                updateGame();
+                lastUpdate = System.nanoTime();
+                updates++;
+            }
+
+            if (System.currentTimeMillis() - lastTimeCheck >= 1000) {
+                System.out.println("FPS: " + frames + " | UPS: " + updates);
+                frames = 0;
+                updates = 0;
+                lastTimeCheck = System.currentTimeMillis();
+            }
+
+        }
+
+    }
+    
 }
--- src/GameScreen.java
+++ src/GameScreen.java
@@ -1,79 +1,64 @@
-import javax.swing.JPanel;
-
-import java.awt.image.BufferedImage;
-import java.awt.Graphics;
 import java.awt.Color;
-
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.Random;
+import javax.swing.JPanel;
 
 public class GameScreen extends JPanel {
 
     private Random random;
     private BufferedImage img;
-    private long lastTime;
-    private int frames;
+
 
     private ArrayList<BufferedImage> sprites = new ArrayList<>();
 
-    public GameScreen(BufferedImage img){
+    public GameScreen(BufferedImage img) {
         this.img = img;
 
         loadSprites();
-        
+
         random = new Random();
 
-
     }
-    //320x96
-    //function to load the sprites
+
     private void loadSprites() {
-        for(int y = 0; y < 10; y++){
-            for (int x = 0; x < 10; x++){
-                //looping through the grid of the image
-                //adding every 32 x 32 pixels to the sprites
+
+        for (int y = 0; y < 10; y++) {
+            for (int x = 0; x < 10; x++) {
                 sprites.add(img.getSubimage(x * 32, y * 32, 32, 32));
             }
         }
+
     }
 
-    public void paintComponent(Graphics g){
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        //g.drawImage(sprites.get(19), 0, 0, this);
-
-
-        for(int y = 0; y < 20; y++){
-            for (int x = 0; x < 20; x++){
-                g.drawImage(sprites.get(getRndInt(20)), x * 32, y * 32, null);
+        for (int y = 0; y < 20; y++) {
+            for (int x = 0; x < 20; x++) {
+                g.drawImage(sprites.get(getRndInt()), x * 32, y * 32, null);
             }
         }
 
 
 
-        callFPS();
+
     }
 
-    private void callFPS() {
-        frames++;
-        if(System.currentTimeMillis() - lastTime >= 1000){
-            System.out.println("FPS: " + frames);
-            frames = 0;
-            lastTime = System.currentTimeMillis();
-        }
+
+
+    private int getRndInt() {
+        return random.nextInt(100);
     }
 
-    private int getRndInt(int max){
-        return random.nextInt(max);
+    private Color getRndColor() {
+        int r = random.nextInt(256);
+        int g = random.nextInt(256);
+        int b = random.nextInt(256);
+
+        return new Color(r, g, b);
+
     }
-
-    private Color getRandomColor(){
-        int red = random.nextInt(255);
-        int green = random.nextInt(255);
-        int blue = random.nextInt(255);
-
-        return new Color(red,green,blue);
-    }
-
 
 }
