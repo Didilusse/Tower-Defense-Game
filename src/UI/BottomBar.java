7a1c9f300fe129c24a18c36667258b36266e61cc
Adil Rahmani <didilusse@gmail.com>
2024-10-04 14:13:18 -0400

    added button to go back to menu

--- src/UI/BottomBar.java
+++ src/UI/BottomBar.java
@@ -2,18 +2,62 @@ package UI;
 
 import java.awt.*;
 
+import static main.GameStates.*;
+
+
 public class BottomBar {
     private int x, y, width, height;
+
+    private MyButton bMenu;
 
     public BottomBar(int x, int y, int width, int height) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
+
+        initButtons();
+    }
+
+    private void initButtons() {
+        bMenu = new MyButton("Menu", 2, 642, 100, 30);
+    }
+
+    private void drawButtons(Graphics g) {
+        bMenu.draw(g);
     }
 
     public void draw(Graphics g) {
+        //Background
         g.setColor(new Color(220, 123, 15));
         g.fillRect(x, y, width, height);
+        //Buttons
+        drawButtons(g);
     }
+
+
+    public void mouseClicked(int x, int y) {
+        if(bMenu.getBounds().contains(x, y)) {
+            SetGameState(MENU);
+        }
+    }
+    public void mouseMoved(int x, int y) {
+        bMenu.setMouseOver(false);
+        if(bMenu.getBounds().contains(x, y)) {
+            bMenu.setMouseOver(true);
+        }
+    }
+
+    public void mousePressed(int x, int y) {
+        if(bMenu.getBounds().contains(x, y)) {
+            bMenu.setMousePressed(true);
+        }
+    }
+    public void mouseReleased(int x, int y) {
+        if(bMenu.getBounds().contains(x, y)) {
+            bMenu.resetBooleans();
+        }
+    }
+
+
 }
--- src/scenes/Playing.java
+++ src/scenes/Playing.java
@@ -1,10 +1,8 @@
 package scenes;
 
-import java.awt.Color;
 import java.awt.Graphics;
 
 import UI.BottomBar;
-import UI.MyButton;
 import helper.LevelBuild;
 import main.Game;
 import managers.TileManager;
@@ -13,26 +11,16 @@ public class Playing extends GameScene implements SceneMethods {
 
     private int[][] lvl;
     private TileManager tileManager;
-    private MyButton bMenu;
 
     private BottomBar bottomBar;
 
     public Playing(Game game) {
         super(game);
-        initButtons();
         lvl = LevelBuild.getLevelData();
         tileManager = new TileManager();
         bottomBar = new BottomBar(0, 640, 640, 100);
-
         //The level
-
-
     }
-
-    private void initButtons() {
-        bMenu = new MyButton("Menu", 2, 2, 100, 30);
-    }
-
 
     @Override
     public void render(Graphics g) {
@@ -42,31 +30,33 @@ public class Playing extends GameScene implements SceneMethods {
                 g.drawImage(tileManager.getSprite(id), x * 32, y * 32, null);
             }
         }
-        drawButtons(g);
-        bottomBar.draw(g );
+        bottomBar.draw(g);
     }
 
-    private void drawButtons(Graphics g) {
-        bMenu.draw(g);
-    }
 
     @Override
     public void mouseClicked(int x, int y) {
-
+        if(y>=640){
+            bottomBar.mouseClicked(x,y);
+        }
     }
 
     @Override
     public void mouseMoved(int x, int y) {
-
+        if(y>=640){
+            bottomBar.mouseMoved(x,y);
+        }
     }
 
     @Override
     public void mousePressed(int x, int y) {
-
+        if(y>=640){
+            bottomBar.mousePressed(x,y);
+        }
     }
     @Override
     public void mouseReleased(int x, int y) {
-
+        bottomBar.mouseReleased(x,y);
     }
 
 }
