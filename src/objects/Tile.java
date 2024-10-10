5b8e781074fe054612626ee3ea1139d22846c001
Adil Rahmani <didilusse@gmail.com>
2024-10-10 12:35:45 -0400

    Update Tile.java

--- src/objects/Tile.java
+++ src/objects/Tile.java
@@ -4,18 +4,29 @@ import java.awt.image.BufferedImage;
 
 public class Tile {
 
-    private BufferedImage sprite;
+    private BufferedImage[] sprite;
     private int id;
     private String name;
 
     public Tile(BufferedImage sprite, int id, String name) {
+        this.sprite = new BufferedImage[1];
+        this.sprite[0] = sprite;
+        this.id = id;
+        this.name = name;
+    }
+
+    public Tile(BufferedImage[] sprite, int id, String name) {
         this.sprite = sprite;
         this.id = id;
         this.name = name;
     }
 
+    public BufferedImage getSprite(int animationIndex){
+        return sprite[animationIndex];
+    }
+
     public BufferedImage getSprite() {
-        return sprite;
+        return sprite[0];
     }
 
     public int getId() {
